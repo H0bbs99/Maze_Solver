c746039b4e1c23f27ef7338dab17ad5b341dc776
H0bbs99 <jdoakes13@gmail.com>
2025-02-22 10:25:57 -0600

    Merged window and drawing into graphics

--- graphic.py
+++ graphic.py
@@ -1,5 +1,5 @@
 from tkinter import Tk, BOTH, Canvas
-from drawing import Point, Line
+
 
 class Window():
     def __init__(self, width, height):
@@ -35,4 +35,18 @@ class Window():
         self.__window_running = False
 
 
+
+
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line():
+    def __init__(self, point_a, point_b):
+        self.point_a = point_a
+        self.point_b = point_b
+
+    def draw(canvas, fill_color):
         
+        canvas.create_line(self.point_a.x,self.point_x.y, self.point_b.x, self.point_b.y, fill=fill_color, width=2)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from window import Window
-from drawing import *
+from graphic import Window, Point, Line
+
 
 def main():
     win = Window(800, 600)
